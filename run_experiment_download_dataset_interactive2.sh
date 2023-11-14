8dcd47db0df486cfaf67fcda3a108c583d6b36c4
ylrsh wa <ylrshwa@MacBook-Pro-324.local>
2023-11-14 00:58:41 +0000

    change config debug1

---
+++ run_experiment_download_dataset_interactive2.sh
@@ -0,0 +1,24 @@
+. /etc/profile.d/modules.sh
+module unload cuda
+module load cuda/12.1.1
+
+export TRANSFORMERS_OFFLINE=1
+export TRANSFORMERS_CACHE=/exports/eddie/scratch/s2522559/cache
+export HF_DATASETS_CACHE=/exports/eddie/scratch/s2522559/cache
+
+python modify_running_script.py \
+  --overwrite_output_dir \
+  --job_dir=../experiments \
+  --push_to_hub false \
+  --streaming true \
+  --do_eval false \
+  --text_renderer_name_or_path=configs/renderers/noto_renderer \
+  --mask_ratio=0.25 \
+  --span_mask \
+  --masking_max_span_length=6 \
+  --masking_cumulative_span_weights=0.2,0.4,0.6,0.8,0.9,1.0 \
+  --dropout_prob=0.1 \
+  --training_config_name=fp16_apex_bs32 \
+  --nodes 1 \
+  --ngpus 1 \
+  # --partition gpu
